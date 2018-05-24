f9a3029ba66e9ef1e474808064200e9e154bc34a
lcarranco <lcarranco@gmail.com>
2018-05-24 15:08:40 -0500

    Created pages of book

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+vector<int> read_ints(int n) {
+    vector<int> ints;
+    for (int i = 1; i <= n; ++i) {
+        ints.push_back(i);
+        // cout << ints[i] << ' ';
+    }
+    return ints;
+}
+
 int main() {
-    cout << "Hello World!" << endl;
+    int n = 0;
+    cin >> n;
+
+    vector<int> ints = read_ints(n);
+
+    for (vector<int>::iterator it = ints.begin(); it != ints.end(); ++it) {
+        cout << *it << ' ';
+    }
+
+    cout << endl;
     return 0;
 }
